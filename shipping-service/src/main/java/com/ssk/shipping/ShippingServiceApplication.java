b006d52b54d69e4206789b066bbfe2847ea54867
CE00120220 <goldy.sarraf@mapmyindia.com>
2026-09-24 11:11:23 +0530

    Pushed Correct Project to its Correct repo

--- shipping-service/src/main/java/com/ssk/shipping/ShippingServiceApplication.java
+++ shipping-service/src/main/java/com/ssk/shipping/ShippingServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShippingServiceApplication {
     public static void main(String[] args) {
         System.out.println("=====================================================");
-        System.out.println("    SSK SHIPPING & LOGISTICS (PORT 8087) STARTING... ");
+        System.out.println("    SSK SHIPPING & LOGISTICS (PORT 8087) STARTING!... ");
         System.out.println("=====================================================");
         SpringApplication.run(ShippingServiceApplication.class, args);
     }
